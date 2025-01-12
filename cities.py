14cfd75a06eed6b0f942a98472df9a64b6bf390d
Mwala Z Mulenga <mwalamulenga.zt@gmail.com>
2025-01-12 13:13:37 +0200

    update db

---
+++ cities.py
@@ -0,0 +1,7 @@
+from province import Province
+
+
+class Cities(Province):
+    def __init__(self, name: str):
+        self.name = name
+
---
+++ province.py
@@ -0,0 +1,4 @@
+class Province:
+    def __init__(self):
+        self.city = []
+
