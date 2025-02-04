80711b41f9c63c46947431650184bc927c940e21
Mwala Z Mulenga <mwalamulenga.zt@gmail.com>
2025-02-04 12:46:11 +0200

    feat: update model

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# scope-zm
+# Scope Zambia
+
+Scope is a Python library for easily finding locations in Zambia. These can be used to fill in forms etc
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install scope_zm
+```
+
+## Usage
+
+```python
+import scope_zm
+
+# returns 'Lusaka province'
+scope.find_province_by_city('lusaka')
+
+# returns '["towns]'
+foobar.towns_in_central()
+
+# returns 'phenomenon'
+foobar.singularize('phenomena')
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- scope/model.py
+++ scope/model.py
@@ -3,26 +3,71 @@ import yaml
 with open("scope/db/db.yml", "r") as f:
     data = yaml.full_load(f)
 
+
 class ScopeZM:
-    get_provinces = ""
+    all_provinces = [p["name"] for p in data]
 
-    def __init__(self, provinces, cities) -> None:
+    def __init__(self, provinces=all_provinces) -> None:
         self.provinces = provinces
-        self.cities = cities
+        self.size = len(provinces)
 
-    def provinces(self):
-        for p in data:
-            return p["name"]
+    def get_all_provinces(self) -> list:
+        return self.provinces
 
     def current_province(self):
         pass
 
-    def cities(self, province):
-        for cities in province:
-            return cities
+    def cities(self, province: str) -> list:
+        for p in data:
+            if province.lower() == p["name"] or province.lower() in p["name"]:
+                return sorted(p["towns"])
+        return []
+
+    def number_of_provinces(self) -> int:
+        return self.size
+
+    # Get towns in each province
+    def towns_in_central(self) -> list:
+        return sorted(data[0]["towns"])
+
+    def towns_in_copperbelt(self) -> list:
+        return sorted(data[1]["towns"])
+
+    def towns_in_eastern(self) -> list:
+        return sorted(data[2]["towns"])
+
+    def towns_in_luapula(self) -> list:
+        return sorted(data[3]["towns"])
+
+    def towns_in_lusaka(self) -> list:
+        return sorted(data[4]["towns"])
+
+    def towns_in_muchinga(self) -> list:
+        return sorted(data[5]["towns"])
+
+    def towns_in_northern(self) -> list:
+        return sorted(data[6]["towns"])
+
+    def towns_in_north_western(self) -> list:
+        return sorted(data[7]["towns"])
+
+    def towns_in_southern(self) -> list:
+        return sorted(data[8]["towns"])
+
+    def towns_in_western(self) -> list:
+        return sorted(data[9]["towns"])
+
+    # to search for province by city
+    def find_province_by_city(self, city: str) -> str:
+        for p in data:
+            if city.capitalize() in p["towns"]:
+                return f"{p["name"]}".capitalize()
+        return f"{city} was not found. Check you spelling"
 
     def get(self, province=None, city=None):
-        if province == None:
-            return self.provinces
-        elif city == None:
-            return self.cities
+        if province is None and city is None:
+            return []
+        if province is None:
+            return self.find_province_by_city(city=str(city))
+        if city is None:
+            return self.get_all_provinces()
